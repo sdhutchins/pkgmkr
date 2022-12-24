65ddcc22fee421de932c5c56e8ab99a1e254ba94
sdhutchins <sdhutchins@uab.edu>
2022-12-24 02:32:40 -0600

    Add mk_pkg_from_config function.
    Refactor mk_pkg function.

--- R/mk_pkg.R
+++ R/mk_pkg.R
@@ -1,50 +1,92 @@
 #' @title Make Package
 #'
-#' @description FUNCTION_DESCRIPTION
+#' @description Create a new R package with the specified parameters
 #'
-#' @param path DESCRIPTION.
-#' @param author DESCRIPTION.
-#' @param git_username DESCRIPTION.
-#' @param email DESCRIPTION.
-#' @param readme_md DESCRIPTION.
-#' @param git DESCRIPTION.
-#' @param check_pkg_name DESCRIPTION.
-#' @param ci DESCRIPTION.
-#' @param license DESCRIPTION.
-#' @param pkgdown DESCRIPTION.
+#' @param path character. The path to create the package at.
+#' @param author character. The name of the package author.
+#' @param git_username character. The username to use for Git and GitHub.
+#' @param email character. The email to use for Git and GitHub.
+#' @param readme_md logical. Whether to create a README file in markdown format.
+#' @param git logical. Whether to initialize a Git repository for the package.
+#' @param check_pkg_name logical. Whether to check that the package name is available on CRAN.
+#' @param license character. The license to use for the package (either "MIT" or "GPL-3").
+#' @param pkgdown logical. Whether to create a pkgdown site for the package.
 #' @import usethis
 #' @importFrom available available
-#' @return RETURN_DESCRIPTION
+#' @return NULL
 #' @export
-mk_package <- function(path, author, email, git_username = NULL, git_email = NULL, readme_md = TRUE, git = TRUE, check_pkg_name=TRUE, ci="travis", license="MIT",
-                       pkgdown=TRUE) {
-    p <- user_path_prep(path)
-    usethis::create_package(path=path, open = FALSE)
-    names <- as.list(strsplit(author, split = " ")[[1]])
-    usethis::use_description(fields = list(
-        `Authors@R` = paste0('person("Jane", "Doe", email = "jane@example.com", role = c("aut", "cre"))')))
-    setwd(p)
-    if (check_pkg_name) {
-        available::available(name=p, browse = FALSE)
-    }
 
-    if (readme_md) {
-        usethis::use_readme_md()
-    }
-    if (license == "mit") {
-        usethis::use_mit_license(name=name)
-    }
-    if (ci == "travis") {
-        usethis::use_travis()
-    }
-    if (git) {
-        usethis::use_git()
-        use_git_config(scope = "project", user.name = git_username, user.email = git_email)
-        usethis::use_github()
-    }
-    if (pkgdown) {
-        usethis::use_pkgdown()
-        pkgdown::build_site()
-    }
+mk_pkg <- function(path, author, email, git = TRUE, git_username = NULL, git_email = NULL, readme_md = TRUE, check_pkg_name = TRUE, license = "MIT", pkgdown = TRUE) {
+  # Use the usethis::create_package function to create the package
+  usethis::create_package(path = path, rstudio = TRUE, open = FALSE)
 
+  # Split the author's name into first and last name
+  names <- as.list(strsplit(author, split = " ")[[1]])
+
+  # Set the working directory to the package's path
+  setwd(path)
+
+  # Use the usethis::use_description function to set the package's Authors@R field
+  usethis::use_description(fields = list(
+    `Authors@R` = paste0('person("', names[1], '", "', names[2], '", email = "', email, '", role = c("aut", "cre"))')
+  ))
+
+  # If check_pkg_name is TRUE, check if the package name is available on CRAN
+  if (check_pkg_name) {
+    print(path)
+    available::available(name = path, browse = FALSE)
+  }
+
+  # If readme_md is TRUE, use the usethis::use_readme_md function to create a README file in markdown format
+  if (readme_md) {
+    usethis::use_readme_md()
+  }
+
+  # If license is "MIT", use the usethis::use_mit_license function to set the package's license to MIT
+  if (license == "MIT") {
+    usethis::use_mit_license(copyright_holder = author)
+  }
+
+
+  # If git is TRUE, use the usethis::use_git function to initialize a Git repository for the package, then use the use_git_config function to set the user name and email for Git and GitHub.
+  if (git) {
+    usethis::use_git()
+    usethis::use_git_config(scope = "project", user.name = git_username, user.email = git_email)
+    usethis::use_github()
+  }
+
+  # If pkgdown is TRUE, use the usethis::use_pkgdown function to set up pkgdown for the package, then use the pkgdown::build_site function to build the pkgdown site.
+  if (pkgdown) {
+    usethis::use_pkgdown()
+    pkgdown::build_site()
+  }
+}
+
+#' @title Make Package from Config
+#'
+#' @description Create a package based on a configuration file.
+#'
+#' @param config_path character. The path to the configuration file.
+#' @param file_type character. The file type of the configuration file (either "yaml" or "json").
+#' @return NULL
+#' @export
+mk_pkg_from_config <- function(config_path, file_type = "yaml") {
+  # Use the import_config function to import the configuration data
+  config_data <- pkgmkr::import_config(config_path, file_type)
+
+  # Extract the configuration data from the config_data list
+  pkg_name <- config_data$pkg_name
+  first_name <- config_data$first_name
+  last_name <- config_data$last_name
+  readme_md <- config_data$readme_md
+  git <- config_data$git
+  check_pkg_name <- config_data$check_pkg_name
+  license <- config_data$license
+  pkgdown <- config_data$pkgdown
+
+  # Concatenate the first_name and last_name into a single string for the author argument
+  author <- paste(first_name, last_name, sep = " ")
+
+  # Call the mk_package function with the appropriate arguments
+  mk_pkg(path = pkg_name, author = author, email = NULL, readme_md = readme_md, git = git, check_pkg_name = check_pkg_name, license = license, pkgdown = pkgdown)
 }
